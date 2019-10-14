75b1a3b03f32e989ef40ac8abc0fb72d22da8e25
Jiri Hubacek <hubacekjirka@gmail.com>
2019-10-15 01:49:17 +1100

    get file from s3 and resize

--- code/handler.py
+++ code/handler.py
@@ -1,5 +1,17 @@
 import requests
+import boto3
+import io
+import PIL.Image
+import os
+
+s3 = boto3.client('s3')
+thumbnailSize = int(os.environ['THUMBNAIL_SIZE'])
+
+
 def createThumbnail(event, context):
+    """
+    Executed by an S3 trigger create file event
+    """
     print("event:", event, "context:", context)
     
     try:
@@ -15,7 +27,24 @@ def createThumbnail(event, context):
         print(e)
         return("General error")
 
+    img = getS3Image(bucket,s3ObjectKey)
+    print(img.size)
+
+    imgThumbnail = getImageThumbnail(img)
+    print(imgThumbnail.size)
 
     print("finished")
-    r = requests.get("http://www.google.com")
-    print(r.status_code)
+
+def getS3Image(bucket, s3ObjectKey):
+    response = s3.get_object(Bucket=bucket, Key=s3ObjectKey)
+    imageContent = response['Body'].read()
+
+    file = io.BytesIO(imageContent)
+    img = PIL.Image.open(file)
+    return img
+
+def getImageThumbnail(img):
+    return img.resize(
+        size = (thumbnailSize,thumbnailSize),
+        resample = PIL.Image.LANCZOS
+    )
